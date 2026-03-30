33e3230d7b2cb7ade586a9d628199b88ff4e5b3c
SantosGGV <santitos3wp@gmail.com>
2026-03-30 20:20:53 +0200

    feat(stack): Implementa clase Stack con métodos push, pop y peek
    - Implementa la clase Stack con métodos push, pop y peek
    - Agrega docstrings a la clase y métodos

--- modulo.py
+++ modulo.py
@@ -1,21 +1,24 @@
-def temperatura_a_fahrenheit(celsius):
-    """Convierte temperatura de Celsius a Fahrenheit.
+class Stack:
+    """Clase que implementa una pila con métodos push, pop y peek."""
 
-    Args:
-        celsius (float): Temperatura en Celsius.
+    def __init__(self):
+        """Inicializa la pila vacía."""
+        self.elementos = []
 
-    Returns:
-        float: Temperatura en Fahrenheit.
-    """
-    return (celsius * 9/5) + 32
+    def push(self, elemento):
+        """Agrega un elemento a la pila."""
+        self.elementos.append(elemento)
 
-def temperatura_a_celsius(fahrenheit):
-    """Convierte temperatura de Fahrenheit a Celsius.
+    def pop(self):
+        """Elimina y devuelve el último elemento de la pila. Si la pila está vacía, levanta una excepción IndexError."""
+        if self.elementos:
+            return self.elementos.pop()
+        else:
+            raise IndexError('Pila vacía')
 
-    Args:
-        fahrenheit (float): Temperatura en Fahrenheit.
-
-    Returns:
-        float: Temperatura en Celsius.
-    """
-    return (fahrenheit - 32) * 5/9
+    def peek(self):
+        """Devuelve el último elemento de la pila sin eliminarlo. Si la pila está vacía, levanta una excepción IndexError."""
+        if self.elementos:
+            return self.elementos[-1]
+        else:
+            raise IndexError('Pila vacía')
